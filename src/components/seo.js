5053e1089447d75669ff09abd0fc5ad8254bff25
Inovact <72455823+Inovact@users.noreply.github.com>
2022-02-19 11:59:25 +0530

    Title change

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Head from 'next/head';
 
 export default function SEO({
-  description = 'startup apps landing page',
-  author = 'RedQ, Inc',
+  description = 'inovact landing page',
+  author = 'Inovact',
   meta,
-  title = 'startup landing title',
+  title = 'inovact',
 }) {
   const metaData = [
     {
